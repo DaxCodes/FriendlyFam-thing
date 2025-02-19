0f85955524dbac94e5476508da31e96b20557b4b
Daxton B. <93626895+DaxCodes@users.noreply.github.com>
2025-02-19 02:37:08 +0000

    hi

--- main.py
+++ main.py
@@ -117,4 +117,10 @@ def logout():
     return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
